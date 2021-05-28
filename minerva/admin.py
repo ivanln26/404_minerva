852c5eadb357907298b48e5416c554c3c69d71a1
Tomasberretta1 <tomi.berretta@gmail.com>
2021-05-28 20:11:52 -0300

    Create models

--- minerva/admin.py
+++ minerva/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Client, Vendor, Category, Product, Sale, Sale_Product
 
 # Register your models here.
+@admin.register(Vendor)
+class VendorAdmin(admin.ModelAdmin):
+    pass 
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Sale)
+class SaleAdmin(admin.ModelAdmin):
+    pass
+@admin.register(Sale_Product)
+class Sale_ProductAdmin(admin.ModelAdmin):
+    pass
--- minerva/models.py
+++ minerva/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class Vendor(AbstractUser):
+    phone = models.CharField(max_length=20)
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    dni = models.CharField(max_length=10)
+    phone = models.CharField(max_length=20, blank=True)
+    email = models.EmailField(blank=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+class Product(models.Model):
+    name = models.CharField(max_length=25)
+    price = models.FloatField()
+    stock = models.IntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+class Sale(models.Model):
+    state = models.CharField(max_length=20)
+    vendor = models.ForeignKey(Vendor, on_delete=models.SET_NULL, null=True)
+    client = models.ForeignKey(Client, on_delete=models.SET_NULL, null=True)
+    location = models.CharField(max_length=20)
+    create_at = models.DateField(auto_now_add=True)
+
+class Sale_Product(models.Model):
+    sale = models.ForeignKey(Sale, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    quantity = models.IntegerField()
+    unitary_price = models.FloatField()
+    
